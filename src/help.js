3ebf5ec39c1315d4fa18703f94773b9e38b878cb
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-05-09 16:00:23 +0800

    Update help text

--- src/help.js
+++ src/help.js
@@ -1,3 +1,11 @@
+const helpText = `
+Hi Shaiful! This bot can help you track your expense.
+
+*Record transaction*
+/add to add money to accounts
+/buy to records expense transaction.
+`;
+
 function help(id) {
-  sendText(id, "Show help here.")
+  sendText(id, helpText);
 }
