fba59d443d7ec0a86bc6fa391da3496befedbb08
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-06-19 09:29:28 +0800

    Add webpack plugin to output HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const GasPlugin = require('gas-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -13,5 +14,8 @@ module.exports = {
   module: {
     rules: [{ test: /\.ts?$/, loader: 'awesome-typescript-loader' }],
   },
-  plugins: [new GasPlugin()],
+  plugins: [
+    new GasPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html', inject: false }),
+  ],
 };
