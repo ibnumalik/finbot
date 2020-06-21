2a43b63126383d7f6525429282f9606cd972b23f
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-06-21 12:54:50 +0800

    Make date parser work

--- src/app/parser.spec.ts
+++ src/app/parser.spec.ts
@@ -1,18 +1,35 @@
 import { Parser } from './parser';
 
-describe('Parser', () => {
-  const text = `add $400 to @cimb +bonus`;
-  const parser = new Parser(text);
+const basicCommand = `add $400 to @cimb +bonus`;
+const commandWithDate = `add $400 to @cimb at 20/6/2020 +freelance`;
 
-  test('should have original text', () => {
-    expect(parser.originalText).toBe(text);
+describe('Parser', () => {
+  const parser = new Parser(basicCommand);
+
+  it('should have original text', () => {
+    expect(parser.originalText).toBe(basicCommand);
   });
 
-  test('should tokenize text', () => {
+  it('should tokenize text', () => {
     expect(parser.token.length).toBe(5);
   });
 
-  test('should define action as first string in text', () => {
+  it('should define action as first string in text', () => {
     expect(parser.getFirstString()).toBe('add');
+  });
+
+  describe('without date', () => {
+    it('should get today date', () => {
+      jest.spyOn(Date, 'now').mockImplementation(() => new Date().valueOf());
+      expect(parser.getDates()).toEqual(new Date());
+    });
+  });
+
+  describe('with date', () => {
+    const parserWithDate = new Parser(commandWithDate);
+
+    it('should get date defined by user', () => {
+      expect(parserWithDate.getDates()).toBe('20/6/2020');
+    });
   });
 });
--- src/app/parser.ts
+++ src/app/parser.ts
@@ -1,6 +1,9 @@
 export class Parser {
-  /* The tokenize string command . */
+  /* The tokenize string command. */
   public token: string[];
+
+  /* The tokenize command that is not manipulated. */
+  public originalToken: string[];
 
   /* The original text message from user. */
   public originalText: string;
@@ -9,9 +12,50 @@ export class Parser {
     this.originalText = text;
 
     this.token = text.split(' ');
+    this.originalToken = this.token;
   }
 
   public getFirstString(): string {
     return this.token[0];
+  }
+
+  /**
+   * Get the date of the transaction. Default to current date.
+   * ['at', '20/6/2020']
+   */
+  public getDates(): string | Date {
+    const dateIndex = this.getStringIndex('at');
+
+    return dateIndex > -1 ? this.extractValue(dateIndex, 2)[1] : new Date();
+  }
+
+  /**
+   * getAmount
+   */
+  public getAmount() {
+    try {
+      return '@';
+    } catch (error) {
+      throw new Error('');
+    }
+  }
+
+  /**
+   * Get the value from token and delete it.
+   *
+   * @param index The token start index
+   * @param take How much element to delete
+   */
+  public extractValue(index: number, take = 1) {
+    return this.token.splice(index, take);
+  }
+
+  /**
+   * Find index that starts with the given character.
+   *
+   * @param symbol string character
+   */
+  private getStringIndex(symbol: string): number {
+    return this.originalToken.findIndex((str) => str.startsWith(symbol));
   }
 }
